c2ca70e17718a0415ee567c2f56fb75231755c38
V4G4X <varun.gawande@gmail.com>
2019-02-20 19:52:19 +0530

    new file:   Expression Tree/.cproject 	new file:   Expression Tree/.gitignore 	new file:   Expression Tree/.project 	new file:   Expression Tree/.settings/language.settings.xml 	new file:   Expression Tree/src/ExpTree.h 	new file:   Expression Tree/src/Expression Tree.cpp

---
+++ Expression Tree/src/ExpTree.h
@@ -0,0 +1,13 @@
+/*
+ * ExpTree.h
+ *  Created on: 20-Feb-2019
+ */
+
+#ifndef EXPTREE_H_
+#define EXPTREE_H_
+
+
+
+
+
+#endif /* EXPTREE_H_ */
---
+++ Expression Tree/src/Expression Tree.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : Expression.cpp
+// Author      : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
