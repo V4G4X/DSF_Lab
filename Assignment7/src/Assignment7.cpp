f78d7b270d0d1192f3fce7d196b3c76be5c109c2
V4G4X <varun.gawande@gmail.com>
2019-03-01 13:17:51 +0530

    renamed:    PQ/.settings/language.settings.xml -> Assignment7/.settings/language.settings.xml 	new file:   Assignment7/src/Assignment7.cpp 	new file:   Assignment7/src/Graph.cpp 	new file:   Assignment7/src/Graph.h 	deleted:    PQ/Release/PQ 	deleted:    PQ/Release/src/subdir.mk 	deleted:    PQ/src/Hospital.cpp 	deleted:    PQ/src/LLADT.h 	deleted:    PQ/src/LL_IMP.cpp 	deleted:    PQ/src/PQ_ADT.h 	deleted:    PQ/src/PQ_IMP.cpp

---
+++ Assignment7/src/Assignment7.cpp
@@ -0,0 +1,13 @@
+//==============================================================
+// Name         : Assignment7.cpp
+// Author   	: Varun Gawande
+// Date			: 1/4/19
+// Description  : Kruskal's Algorithm in C++
+//==============================================================
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
--- PQ/Release/src/PQ_IMP.d
+++
@@ -1,8 +0,0 @@
-src/PQ_IMP.o: ../src/PQ_IMP.cpp ../src/LLADT.h ../src/LL_IMP.cpp \
- ../src/PQ_ADT.h
-
-../src/LLADT.h:
-
-../src/LL_IMP.cpp:
-
-../src/PQ_ADT.h:
